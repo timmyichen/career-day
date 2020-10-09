d8f2b1e5866cb1b2930429a42c4060523f189f68
timchen <timmy.i.chen@gmail.com>
2020-10-09 09:30:18 -0700

    eslint/prettier

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,10 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('yay')
+  res.send('yay');
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`server started on port ${port}`)
-})
+  console.log(`server started on port ${port}`);
+});
