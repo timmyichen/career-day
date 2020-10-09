41a6694781297567eeb3463ee0b27a3a93743514
timchen <timmy.i.chen@gmail.com>
2020-10-09 09:16:36 -0700

    basic express server

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('yay')
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
+})
