72935910f2dcb91492ad24c96a27aef355704719
aankit <aankit.patel@gmail.com>
2020-10-14 23:06:45 -0400

    fixing error in companies model

--- server/models/Companies.ts
+++ server/models/Companies.ts
@@ -11,7 +11,7 @@ export default class Companies extends BaseEntity {
   @Column({ length: 256, nullable: false })
   location!: string;
 
-  @Column({type: 100, nullable: false})
+  @Column({ length: 100, nullable: false})
   industry!: string;
   
   @Column({ type: 'text', default: '' })
